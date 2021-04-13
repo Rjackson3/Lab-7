8fbc01860a2ca55f4bfdd44956e0d7438f26eaec
Rjackson3 <77811493+Rjackson3@users.noreply.github.com>
2021-04-13 18:09:03 -0500

    rewrote java script

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function init() {
 //add your javascrip between these two lines of code
   var entry = document.getElementById("entrybutton");
-  function myFunction() {
+  function myScript() {
     var textBox = document.getElementById("entryinput");
     var output = document.getElementById("textoutput");
     alert("Ryan Jackson: " + textBox.value);
